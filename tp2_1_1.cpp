36671eae0b08e9d7f8f5dcec4893908e1d9a3107
PavonEvelin <evelinluz17@gmail.com>
2020-04-07 17:55:16 -0300

    agregado del archivo tp2_1_1.cpp

---
+++ tp2_1_1.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#define N 4
+#define M 5
+
+int main(){
+ 
+   Int f,c;
+   Double mt[N][M];
+
+   for(f = 0;f<N; f++){
+
+       for(f = 0;f<M; f++){
+
+           printf(“%lf ”, mt[f][c]);   
+
+       }
+
+       printf(“\n”);
+   }
+
+}
